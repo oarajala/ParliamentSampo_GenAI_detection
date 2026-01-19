1cd61d26c6fe9a6fe3a39ac197379d706bb1a7ab
oarajala <otso.rajala@helsinki.fi>
2026-01-19 17:20:03 +0200

    Staged data in analysis phase.

--- project/03_analysis.py
+++ project/03_analysis.py
@@ -14,6 +14,15 @@ directory = helpers.get_parent_directory()
 freq = pd.read_csv(f'{directory}/csv_analysis/word_frequency_all_years.csv', sep=';', encoding='utf-8')
 
 # note: small peeks into the set at this point
+df = freq.loc[(freq['norm_2023']>freq['norm_2022'])&(freq['norm_2024']>freq['norm_2022'])&(freq['norm_2025']>freq['norm_2022'])
+              &(freq['norm_2023']>freq['norm_2023_predicted'])&(freq['norm_2024']>freq['norm_2024_predicted'])&(freq['norm_2025']>freq['norm_2025_predicted'])].sort_values(by='norm_2025', ascending=False)
+               
+print(df[['word','norm_2021','norm_2022','norm_2023','norm_2023_ratios','norm_2024','norm_2025']].sort_values(by='norm_2023_ratios', ascending=True))
+save_file_name = 'analysis_first_peek.csv'
+if save_file_name in os.listdir(f'{directory}/csv_analysis/'):
+    os.remove(f'{directory}/csv_analysis/{save_file_name}')
+df.to_csv(f'{directory}/csv_analysis/{save_file_name}', sep=';', header=True, index=False, encoding='utf-8')
+
 print(freq[['word', 'n_2021', 'n_2022', 'n_2023_predicted', 'n_2023', 'n_2024_predicted', 'n_2024', 'n_2025_predicted', 'n_2025', 'n_2023_diffs', 'n_2023_ratios',
             'n_2024_diffs', 'n_2024_ratios', 'n_2025_diffs', 'n_2025_ratios']].loc
       [(freq['n_2023'] > freq['n_2023_predicted'])&(freq['n_2024'] > freq['n_2024_predicted'])&(freq['n_2025'] > freq['n_2025_predicted'])]
